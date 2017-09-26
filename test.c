d9737d3a773d7f434d343fcd791ff9604026ca27
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2017-09-26 22:48:17 +0100

    Improve whitespace removal performance of test

--- test.c
+++ test.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 #define MAX_PASSWORD_LEN	32
 
 int main()
 {
 	char password[MAX_PASSWORD_LEN];
+	char *c;
 
 	while(fgets(password, MAX_PASSWORD_LEN, stdin) != NULL)
 	{
-		sscanf(password, "%s", password);
+		// remove \n and \r at the end of lines
+		c = password + strlen(password) - 1;
+		while(isspace(*c)) {
+			*c = 0;
+			c--;
+		}
 		printf("%s\n", password);
 	}
 
